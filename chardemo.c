920f885fbfb7740dfafc5d6e6c4a001a045b3e43
Thomas Cherian <thomascherian2028@cs.sjcetpalai.ac.in>
2025-01-22 16:24:33 +0530

    To convert capital letter to small

---
+++ chardemo.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main() {
+    char value;
+    printf("\nEnter a character in capital letter:");
+    value=getchar();
+    value=value+32;
+    printf("Lowert case letter: %c\n",value);
+    return 0;
+}
+  
