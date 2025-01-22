aae8374b264493cfa7230107209b6ef607321546
Thomas Cherian <thomascherian2028@cs.sjcetpalai.ac.in>
2025-01-22 16:26:14 +0530

    To find sum of two numbers in C

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+  float number1=10,number2=20,sum;
+  printf("\nEnter two integer numbers");
+  scanf("%f%f",&number1,&number2);
+  sum= number1+number2;
+  printf("%f+%f=%f\n",number1,number2,sum);
+return 0;
+}
