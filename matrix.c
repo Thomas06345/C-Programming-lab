3607594d8fe2cf0dfa6304eded3031875dec1900
Thomas Cherian <thomascherian2028@cs.sjcetpalai.ac.in>
2025-02-07 09:44:06 +0530

    program to create a matrix

---
+++ matrix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+  int row,col;
+  printf("enter the row and column:\n");
+  scanf("%d%d",&row,&col);
+  int matrix[row][col];
+  printf("enter the elements:\n");
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+      scanf("%d",&matrix[i][j]);
+    }
+  }
+  printf("The matrix is:\n");
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+      printf("%d\t",matrix[i][j]);
+    }
+  printf("\n");
+ }
+  return 0;
+}
